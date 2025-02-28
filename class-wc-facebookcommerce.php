c2ed1aba0fa4fd66620d09d3061bd227ed3bd0f2
Luka Garic <luka@stuntcoders.com>
2025-02-28 10:33:23 +0100

    Add $admin_settings_asc variable declaration

--- class-wc-facebookcommerce.php
+++ class-wc-facebookcommerce.php
@@ -117,6 +117,9 @@ class WC_Facebookcommerce extends WooCommerce\Facebook\Framework\Plugin {
 	/** @var WooCommerce\Facebook\Products\FBCategories instance. */
 	private $fb_categories;
 
+	/** @var WooCommerce\Facebook\AdvertiseASC\AscSelector */
+	private $admin_settings_asc;
+
 	/**
 	 * The Debug tools instance.
 	 *
