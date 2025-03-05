a3a11098eaf8d467d6453c9e2154e8ad201b9e8f
Julio Mendez Cabrera <jmencab@meta.com>
2025-03-05 13:36:24 -0800

    Move most code from ExampleFeedGenerator to FeedGenerator

--- includes/Feed/ExampleFeedGenerator.php
+++ includes/Feed/ExampleFeedGenerator.php
@@ -16,53 +16,6 @@ defined( 'ABSPATH' ) || exit;
  */
 class ExampleFeedGenerator extends FeedGenerator {
 	/**
-	 * Used to interact with the directory system.
-	 *
-	 * @var FeedFileWriter $feed_writer
-	 */
-	private FeedFileWriter $feed_writer;
-
-	/**
-	 * Constructor for this instance.
-	 *
-	 * @param ActionSchedulerInterface $action_scheduler Global scheduler.
-	 * @param FeedHandler              $feed_handler The feed handler instance for this feed.
-	 */
-	public function __construct( ActionSchedulerInterface $action_scheduler, FeedHandler $feed_handler ) {
-		parent::__construct( $action_scheduler, $feed_handler );
-		$this->feed_writer = $feed_handler->get_feed_writer();
-	}
-
-	/**
-	 * Handles the start of the feed generation process.
-	 *
-	 * @inheritdoc
-	 * @since 3.5.0
-	 */
-	protected function handle_start() {
-		// Create directory if not available and then the files to protect the directory.
-		$this->feed_writer->create_files_to_protect_feed_directory();
-		$this->feed_writer->prepare_temporary_feed_file();
-	}
-
-	/**
-	 * Handles the end of the feed generation process.
-	 *
-	 * @inheritdoc
-	 * @since 3.5.0
-	 */
-	protected function handle_end() {
-		$this->feed_writer->promote_temp_file();
-
-		/**
-		 * Trigger upload from ExampleFeed instance
-		 *
-		 * @since 3.5.0
-		 */
-		do_action( AbstractFeed::FEED_GEN_COMPLETE_ACTION . FeedManager::EXAMPLE );
-	}
-
-	/**
 	 * Retrieves items for a specific batch.
 	 *
 	 * @param int   $batch_number The batch number.
@@ -156,52 +109,5 @@ class ExampleFeedGenerator extends FeedGenerator {
 		} else {
 			return array();
 		}
-	}
-
-	/**
-	 * Processes a batch of items.
-	 *
-	 * @param array $items The items to process.
-	 * @param array $args Additional arguments.
-	 * @inheritdoc
-	 * @since 3.5.0
-	 */
-	protected function process_items( array $items, array $args ) {
-		$this->feed_writer->write_temp_feed_file( $items );
-	}
-
-	/**
-	 * Processes a single item.
-	 *
-	 * @param mixed $item The item to process.
-	 * @param array $args Additional arguments.
-	 * @inheritdoc
-	 * @since 3.5.0
-	 */
-	protected function process_item( $item, array $args ) {
-		// Needed to satisfy the class inheritance
-		// Because of the i/o opening and closing original feed implementation foregoes this method;
-		// It is more efficient to write each batch out and per object processing is done in write_feed_file().
-	}
-
-	/**
-	 * Gets the name of the feed generator.
-	 *
-	 * @return string The name of the feed generator.
-	 * @inheritdoc
-	 * @since 3.5.0
-	 */
-	public function get_name(): string {
-		return self::class;
-	}
-
-	/**
-	 * Gets the batch size for the feed generation process.
-	 *
-	 * @return int The batch size.
-	 * @inheritdoc
-	 */
-	protected function get_batch_size(): int {
-		return 1;
 	}
 }
