d1889d58a388b56d023877b324ea8913bfc7438d
Neal Wei <nealwei@fb.com>
2025-03-07 10:28:21 -0800

    WIP

---
+++ includes/API/MetaLog/Error/Response.php
@@ -0,0 +1,13 @@
+<?php
+declare( strict_types=1 );
+
+namespace WooCommerce\Facebook\API\MetaLog\Error;
+
+use WooCommerce\Facebook\API\Response as ApiResponse;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Response object for MetaLog > Error Graph Api.
+ */
+class Response extends ApiResponse {}
