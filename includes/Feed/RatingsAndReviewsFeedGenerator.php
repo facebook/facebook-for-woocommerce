862944d762f32160b46713cb938e2187f8c334aa
Noah Ostrowski <nrostrow@meta.com>
2025-03-11 14:07:01 -0700

    Moving shared logic into utils file to reduce code duplication

--- includes/Feed/RatingsAndReviewsFeedGenerator.php
+++ includes/Feed/RatingsAndReviewsFeedGenerator.php
@@ -18,7 +18,7 @@ class RatingsAndReviewsFeedGenerator extends FeedGenerator {
 	/**
 	 * Retrieves items for a specific batch.
 	 *
-	 * @param int $batch_number The batch number.
+	 * @param int   $batch_number The batch number.
 	 * @param array $args Additional arguments.
 	 *
 	 * @return array The items for the batch.
@@ -36,55 +36,7 @@ class RatingsAndReviewsFeedGenerator extends FeedGenerator {
 			'status' => 'approve',
 		);
 
-		$comments     = get_comments( $query_args );
-		$reviews_data = array();
-
-		$store_name = get_bloginfo( 'name' );
-		$store_id   = get_option( 'wc_facebook_commerce_merchant_settings_id', '' );
-		$store_urls = [ wc_get_page_permalink( 'shop' ) ];
-
-		foreach ( $comments as $comment ) {
-			$post_type = get_post_type( $comment->comment_post_ID );
-			if ( 'product' !== $post_type ) {
-				continue;
-			}
-
-			$rating = get_comment_meta( $comment->comment_ID, 'rating', true );
-			if ( null === $rating ) {
-				continue;
-			}
-
-			$reviewer_id           = $comment->user_id;
-			$reviewer_is_anonymous = '0' === $reviewer_id ? 'true' : 'false';
-
-			$product = wc_get_product( $comment->comment_post_ID );
-			if ( null === $product ) {
-				continue;
-			}
-			$product_name = $product->get_name();
-			$product_url  = $product->get_permalink();
-			$product_skus = [ $product->get_sku() ];
-
-			$reviews_data[] = array(
-				'aggregator'                      => 'woocommerce',
-				'store.name'                      => $store_name,
-				'store.id'                        => $store_id,
-				'store.storeUrls'                 => "['" . implode( "','", $store_urls ) . "']",
-				'review_id'                       => $comment->comment_ID,
-				'rating'                          => intval( $rating ),
-				'title'                           => null,
-				'content'                         => $comment->comment_content,
-				'created_at'                      => $comment->comment_date,
-				'reviewer.name'                   => $comment->comment_author,
-				'reviewer.reviewerID'             => $reviewer_id,
-				'reviewer.isAnonymous'            => $reviewer_is_anonymous,
-				'product.name'                    => $product_name,
-				'product.url'                     => $product_url,
-				'product.productIdentifiers.skus' => "['" . implode( "','", $product_skus ) . "']",
-			);
-		}
-
-		return $reviews_data;
+		return FeedUploadUtils::get_ratings_and_reviews_data( $query_args );
 	}
 
 	/**
