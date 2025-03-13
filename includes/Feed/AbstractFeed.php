0178d6e5b6070df1362b885856ad6dce0af39c14
Carter Buce <carterbuce@gmail.com>
2025-03-13 16:46:18 -0700

    remove todo

--- includes/Feed/AbstractFeed.php
+++ includes/Feed/AbstractFeed.php
@@ -219,7 +219,7 @@ abstract class AbstractFeed {
 
 		$secret = get_option( $secret_option_name, '' );
 		if ( ! $secret ) {
-			$secret = wp_hash( 'example-feed-' . time() ); // TODO is there a default secret we can use
+			$secret = wp_hash( 'example-feed-' . time() );
 			update_option( $secret_option_name, $secret );
 		}
 
