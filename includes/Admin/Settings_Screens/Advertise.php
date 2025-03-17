c8a1be23531da88fec25536d0ad913796b64e744
Jovana Ranđelović <jovana@stuntcoders.com>
2025-03-17 16:43:26 +0100

    Update render dashboard container params and layout

--- includes/Admin/Settings_Screens/Advertise.php
+++ includes/Admin/Settings_Screens/Advertise.php
@@ -302,7 +302,7 @@ class Advertise extends Abstract_Settings_Screen {
 	 * @param string @subtitle_row2. Row2 of the subtitle of the dashboard
 	 *
 	 */
-	private function render_dashboard( $type, $title, $subtitle_row1, $subtitle_row2 ) {
+	private function render_dashboard( $type, $heading_title, $heading_subtitle, $content ) {
 		$campaign_handler	= facebook_for_woocommerce()->get_advertise_asc_handler($type);
 		$min_daily_budget	= $campaign_handler->get_allowed_min_daily_budget();
 		$currency			= $campaign_handler->get_currency();
@@ -345,14 +345,28 @@ class Advertise extends Abstract_Settings_Screen {
 		?>
 			<div class="main-ui-container">
 				<div class="main-ui-container-item main-ui-container-item-heading">
-					<p class="main-ui-header"><?php echo $title?></p>
+					<h2 class="main-ui-header"><?php echo $heading_title?></h2>
+					<p class="main-ui-subheader"><?php echo $heading_subtitle?></p>
 				</div>
-				<div class="main-ui-container-item main-ui-container-item-content">
-					<p style="line-height: 10px;"><?php echo $subtitle_row1?></p>
-					<p style="line-height: 10px;"><?php echo $subtitle_row2?></p>
+				<div class="main-ui-container-item">
+					<?php
+					foreach ($content as $content_item) {
+						echo "<div class='main-ui-container-item-content'>";
+						foreach ($content_item as $key => $value) {
+							if ($key == 'icon') {
+								echo "<span class='main-ui-container-item-content-icon'>$value</span>";
+							} elseif ($key == 'title') {
+								echo "<h3 class='main-ui-container-item-content-title'>$value</h3>";
+							} else {
+								echo "<p>$value</p>";
+							}
+						}
+						echo "</div>";
+					}
+					?>
 				</div>
 				<div class="main-ui-container-item main-ui-container-item-cta">
-					<button class='components-button is-primary button button-large' id='<?php echo $type?>-create-campaign-btn' disabled>Get Started</button>
+					<button class='components-button button button-large' id='<?php echo $type?>-create-campaign-btn' disabled>Get Started</button>
 				</div>
 			</div>
 		<?php
@@ -420,12 +434,55 @@ class Advertise extends Abstract_Settings_Screen {
 					<table>
 						<tr>
 							<td>
-								<?php $this->render_dashboard(self::ASC_CAMPAIGN_TYPE_NEW_BUYERS, "Find new customers", "Reach out to potential new buyers for your products", "using Advantage+ Shopping (ASC)"); ?>
+								<?php
+								 $new_buyers_content = array(
+									(object) [
+										'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
+									</svg>
+									',
+										'title' => 'Run ads that work for you',
+										'description' => 'Reach out to potential new buyers using Advantage+ Shopping (ASC).'
+									],
+									(object) [
+										'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+										</svg>
+
+									',
+										'title' => 'Reach new customers',
+										'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+									],
+								  );
+
+								 $this->render_dashboard(self::ASC_CAMPAIGN_TYPE_NEW_BUYERS, "Create Campaign", "Reach likely customers with your products, features, and services in the best ad formats.", $new_buyers_content); ?>
 							</td>
 						</tr>
 						<tr>
 							<td>
-								<?php $this->render_dashboard(self::ASC_CAMPAIGN_TYPE_RETARGETING, "Engage with your website visitors", "Bring back visitors who visited your website and didn't complete", "their purchase using Advantage+ Catalog (DPA)"); ?>
+								<?php
+								$retargeting_content = array(
+									(object) [
+										'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+</svg>
+
+										',
+										'title' => 'Retarget shoppers',
+										'description' => "Reach people who've shown interest in products on your website or app and remind them about products they viewed but didn't buy."
+									],
+									(object) [
+										'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+</svg>
+
+									',
+										'title' => 'Access catalogue creative options',
+										'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+									],
+								);
+
+								$this->render_dashboard(self::ASC_CAMPAIGN_TYPE_RETARGETING, "Engage Your Website Visitors", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", $retargeting_content); ?>
 							</td>
 						</tr>
 					</table>
