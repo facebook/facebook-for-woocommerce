21a27973a3fe12b6a9a761aa9bac10608b943819
woo-ardsouza <ardsouza@meta.com>
2025-04-04 17:51:55 -0700

    [WAUM][1/n] Add scaffold for managing utility event config
    Build the utility message overview UI  for the utility messaging tab. Currently, diff is a scafold for the UI elements

--- includes/Admin/Settings_Screens/Whatsapp_Utility.php
+++ includes/Admin/Settings_Screens/Whatsapp_Utility.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
  *
@@ -10,7 +11,7 @@
 
 namespace WooCommerce\Facebook\Admin\Settings_Screens;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 use WooCommerce\Facebook\Admin\Abstract_Settings_Screen;
 use WooCommerce\Facebook\Framework\Api\Exception as ApiException;
@@ -18,29 +19,34 @@ use WooCommerce\Facebook\Framework\Api\Exception as ApiException;
 /**
  * The Whatsapp Utility settings screen object.
  */
-class Whatsapp_Utility extends Abstract_Settings_Screen {
+class Whatsapp_Utility extends Abstract_Settings_Screen
+{
 
 
 	/** @var string screen ID */
 	const ID = 'whatsapp_utility';
 
+	/** @var flag to test Utility Messages Overview changes until check for integration config is implemented */
+	const WHATSAPP_UTILITY_MESSAGES_OVERVIEW_FLAG = false;
 
 	/**
 	 * Whatsapp Utility constructor.
 	 */
-	public function __construct() {
-		add_action( 'init', array( $this, 'initHook' ) );
+	public function __construct()
+	{
+		add_action('init', array($this, 'initHook'));
 
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_assets'));
 	}
 
 	/**
 	 * Initializes this whatsapp utility settings page's properties.
 	 */
-	public function initHook(): void {
+	public function initHook(): void
+	{
 		$this->id    = self::ID;
-		$this->label = __( 'WhatsApp Utility', 'facebook-for-woocommerce' );
-		$this->title = __( 'Whatsapp Utility', 'facebook-for-woocommerce' );
+		$this->label = __('WhatsApp Utility', 'facebook-for-woocommerce');
+		$this->title = __('Whatsapp Utility', 'facebook-for-woocommerce');
 	}
 
 	/**
@@ -50,17 +56,18 @@ class Whatsapp_Utility extends Abstract_Settings_Screen {
 	 *
 	 * @since 2.0.0
 	 */
-	public function enqueue_assets() {
+	public function enqueue_assets()
+	{
 
-		if ( ! $this->is_current_screen_page() ) {
+		if (! $this->is_current_screen_page()) {
 			return;
 		}
 
-		wp_enqueue_style( 'wc-facebook-admin-whatsapp-settings', facebook_for_woocommerce()->get_plugin_url() . '/assets/css/admin/facebook-for-woocommerce-whatsapp-utility.css', array(), \WC_Facebookcommerce::VERSION );
+		wp_enqueue_style('wc-facebook-admin-whatsapp-settings', facebook_for_woocommerce()->get_plugin_url() . '/assets/css/admin/facebook-for-woocommerce-whatsapp-utility.css', array(), \WC_Facebookcommerce::VERSION);
 		wp_enqueue_script(
 			'facebook-for-woocommerce-connect-whatsapp',
 			facebook_for_woocommerce()->get_asset_build_dir_url() . '/admin/whatsapp-connection.js',
-			array( 'jquery', 'jquery-blockui', 'jquery-tiptip', 'wc-enhanced-select' ),
+			array('jquery', 'jquery-blockui', 'jquery-tiptip', 'wc-enhanced-select'),
 			\WC_Facebookcommerce::PLUGIN_VERSION
 		);
 	}
@@ -71,21 +78,82 @@ class Whatsapp_Utility extends Abstract_Settings_Screen {
 	 *
 	 * @since 2.0.0
 	 */
-	public function render() {
 
+	public function render()
+	{
+		if(self::WHATSAPP_UTILITY_MESSAGES_OVERVIEW_FLAG){
+			$this->render_utility_message_overview();
+		}
+		else{
+			$this->render_utility_message_onboarding();
+		}
+		
+        parent::render();
+	}
+
+	public function render_utility_message_onboarding(){
+        ?>
+    <div class="onboarding-card">
+    <h2><?php esc_html_e( 'Get started with WhatsApp utility messages', 'facebook-for-woocommerce' ); ?></h2>
+    <p><?php esc_html_e( 'Connect your WhatsApp Business Account to start sending utility messages.', 'facebook-for-woocommerce' ); ?></p>
+    <a
+            id="woocommerce-whatsapp-connection"
+            class="connect-button"
+            href="#"
+        ><?php esc_html_e( 'Connect Whatsapp Account', 'facebook-for-woocommerce' ); ?></a>
+    </div>
+        <?php
+
+	}
+
+	public function render_utility_message_overview(){
 		?>
-	<div class="onboarding-card">
-	<h2><?php esc_html_e( 'Get started with WhatsApp utility messages', 'facebook-for-woocommerce' ); ?></h2>
-	<p><?php esc_html_e( 'Connect your WhatsApp Business Account to start sending utility messages.', 'facebook-for-woocommerce' ); ?></p>
-	<a
-			id="woocommerce-whatsapp-connection"
-			class="connect-button"
-			href="#"
-		><?php esc_html_e( 'Connect Whatsapp Account', 'facebook-for-woocommerce' ); ?></a>
-	</div>
-		<?php
+		<div class="onboarding-card">
+			<h1><?php esc_html_e('Utility Messages', 'facebook-for-woocommerce'); ?></h2>
+				<p><?php esc_html_e('Manage which utility messages you want to send to customers. You can check performance of these messages in Whatsapp Manager.', 'facebook-for-woocommerce'); ?>
+					<a
+						id="woocommerce-whatsapp-manager-insights"
+						href="#"><?php esc_html_e('View insights', 'facebook-for-woocommerce'); ?></a>
+				</p>
+				<hr />
+				<div style="overflow: hidden">
+					<div style="float: left;">
+						<h3><?php esc_html_e('Order confirmation', 'facebook-for-woocommerce'); ?></h3>
+						<p><?php esc_html_e('Send a confirmation to customers after they\'ve placed an order.'); ?></p>
+					</div>
+					<a
+						id="woocommerce-whatsapp-manage-order-confirmation"
+						class="button"
+						href="#"
+						style="float: right; margin-top: 30px; margin-right: 30px;"><?php esc_html_e('Manage', 'facebook-for-woocommerce'); ?></a>
+				</div>
+				<hr />
+				<div style="overflow: hidden">
+					<div style="float: left;">
+						<h3><?php esc_html_e('Order shipped', 'facebook-for-woocommerce'); ?></h3>
+						<p><?php esc_html_e('Send a confirmation to customers when their order is shipped.'); ?></p>
+					</div>
+					<a
+						id="woocommerce-whatsapp-manage-order-shipped"
+						class="button"
+						href="#"
+						style="float: right; margin-top: 30px; margin-right: 30px;"><?php esc_html_e('Manage', 'facebook-for-woocommerce'); ?></a>
+				</div>
+				<hr />
+				<div style="overflow: hidden">
+					<div style="float: left;">
+						<h3><?php esc_html_e('Order refunded', 'facebook-for-woocommerce'); ?></h3>
+						<p><?php esc_html_e('Send a confirmation to customers when an order is refunded.'); ?></p>
+					</div>
+					<a
+						id="woocommerce-whatsapp-manage-order-refunded"
+						class="button"
+						href="#"
+						style="float: right; margin-top: 30px; margin-right: 30px;"><?php esc_html_e('Manage', 'facebook-for-woocommerce'); ?></a>
+				</div>
+		</div>
+<?php
 
-		parent::render();
 	}
 
 	/**
@@ -96,7 +164,8 @@ class Whatsapp_Utility extends Abstract_Settings_Screen {
 	 *
 	 * @return array
 	 */
-	public function get_settings() {
+	public function get_settings()
+	{
 		return array();
 	}
 }
