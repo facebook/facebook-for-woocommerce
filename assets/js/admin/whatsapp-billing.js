b98bedb19ce64597e94496af3ede49693eb048c3
sharunaanandraj <sanandraj@meta.com>
2025-04-08 22:39:38 -0700

    remove console logs

--- assets/js/admin/whatsapp-billing.js
+++ assets/js/admin/whatsapp-billing.js
@@ -15,9 +15,7 @@ jQuery( document ).ready( function( $ ) {
 			action: 'wc_facebook_whatsapp_fetch_billing_url_info',
 			nonce:  facebook_for_woocommerce_whatsapp_billing.nonce
 		}, function ( response ) {
-            console.log("response 1", response);
             if ( response.success ) {
-                console.log("response 2", response);
                 var  business_id = response.data.business_id;
                 var asset_id = response.data.waba_id;
 				const BILLING_URL = `https://business.facebook.com/billing_hub/accounts/details/?business_id=${business_id}&asset_id=${asset_id}&account_type=whatsapp-business-account`;
