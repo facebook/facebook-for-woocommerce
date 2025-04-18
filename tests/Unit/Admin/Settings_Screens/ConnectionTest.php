bd1ddfabb8dae4f912b7cd64f181df89e2bfc0ee
Angelo Ou <angeloou@meta.com>
2025-04-18 15:12:45 -0400

    Fix more tests

--- tests/Unit/Admin/Settings_Screens/ConnectionTest.php
+++ tests/Unit/Admin/Settings_Screens/ConnectionTest.php
@@ -30,18 +30,9 @@ class ConnectionTest extends TestCase {
     }
 
 	/**
-	 * Test that render method calls render_facebook_iframe when enhanced onboarding is disabled
+	 * Test the rendering of the Facebook connection box
 	 */
 	public function test_render_facebook_box() {
-		// Create a partial mock of the facebook_for_woocommerce() class
-		$fb_for_wc = $this->getMockBuilder(WC_Facebookcommerce::class)
-		                  ->getMock();
-
-		// Override the facebook_for_woocommerce method to return the mock
-		add_filter('wc_facebook_instance', function() use ( $fb_for_wc ) {
-			return $fb_for_wc;
-		});
-
 		// Start output buffering to capture the render output
 		$connection = new Connection();
 		ob_start();
