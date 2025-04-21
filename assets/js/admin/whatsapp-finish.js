1402726010fbc73af0e0e6348ccaba3dc07f3220
sharunaanandraj <sanandraj@meta.com>
2025-04-20 17:14:44 -0700

    finish button ajax call

--- assets/js/admin/whatsapp-finish.js
+++ assets/js/admin/whatsapp-finish.js
@@ -10,14 +10,23 @@
 jQuery( document ).ready( function( $ ) {
     // handle the whatsapp finish button click
 	$( '#wc-whatsapp-onboarding-finish' ).click( function( event ) {
-        // TODO: call the connect API to create configs and check payment
-        // If error, show banner
-        // If success, redirect to utility settings page
-        let url = new URL(window.location.href);
-        let params = new URLSearchParams(url.search);
-        params.set('view', 'utility_settings');
-        url.search = params.toString();
-        window.location.href = url.toString();
+        // call the connect API to create configs and check payment
+        $.post( facebook_for_woocommerce_whatsapp_finish.ajax_url, {
+			action: 'wc_facebook_whatsapp_finish_onboarding',
+			nonce:  facebook_for_woocommerce_whatsapp_finish.nonce
+		}, function ( response ) {
+            if ( response.success ) {
+                // If success, redirect to utility settings page
+                 let url = new URL(window.location.href);
+                 let params = new URLSearchParams(url.search);
+                 params.set('view', 'utility_settings');
+                 url.search = params.toString();
+                 window.location.href = url.toString();
+			} else {
+                // TODO: Handle error show error banner in UI
+                console.log(response);
+            }
+		} );
     });
 
 } );
