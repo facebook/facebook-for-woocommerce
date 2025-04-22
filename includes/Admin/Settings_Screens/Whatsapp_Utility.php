dbb3af45e05a46a3cbfd65a88542bec75dc56173
Ashutosh Bondre <ashubond@meta.com>
2025-04-22 15:47:26 -0700

    update UI card

--- includes/Admin/Settings_Screens/Whatsapp_Utility.php
+++ includes/Admin/Settings_Screens/Whatsapp_Utility.php
@@ -266,22 +266,29 @@ class Whatsapp_Utility extends Abstract_Settings_Screen {
 				</div>
 			</div>
 			<div class="divider"></div>
+
+		</div>
+		<div class="onboarding-card">
 			<div class="card-item event-config">
-				<div>
-					<h3><b><?php esc_html_e( 'Add WhatsApp option at checkout', 'facebook-for-woocommerce' ); ?></b></h3>
-					<p><?php esc_html_e( 'Adds a checkbox to your store\'s checkout page so customers can get updates. You can preview what this looks like in checkout preview.', 'facebook-for-woocommerce' ); ?></p>
-				</div>
-				<div class="event-config-options">
-					<label>
-						<input type="checkbox" checked>
-						<span class="checkbox-label"><?php esc_html_e( 'On', 'facebook-for-woocommerce' ); ?></span>
-					</label>
-					<a
-						id="woocommerce-whatsapp-consent-collection"
-						class="event-config-manage-button button"
-						href="#"><?php esc_html_e( 'Remove', 'facebook-for-woocommerce' ); ?></a>
+					<div>
+						<h3><b><?php esc_html_e( 'Add WhatsApp option at checkout', 'facebook-for-woocommerce' ); ?></b></h3>
+						<p><?php esc_html_e( 'Adds a checkbox to your store\'s checkout page so customers can get updates. You can preview what this looks like in checkout preview.', 'facebook-for-woocommerce' ); ?></p>
+					</div>
+					<div class="divider"></div>
 				</div>
 				<div class="divider"></div>
+				<div class="card-item event-config">
+					<div>
+						<h4><b><?php esc_html_e( 'Checkbox', 'facebook-for-woocommerce' ); ?></b></h4>
+						<p><?php esc_html_e( 'Removing this means you won\'t be able to send messages to your customers.', 'facebook-for-woocommerce' ); ?></p>
+					</div>
+					<div class="event-config-manage-button">
+					<a
+						id="remove-button"
+						class="event-config-manage-button button"
+						href="#"><?php esc_html_e( 'Remove', 'facebook-for-woocommerce' ); ?></a>
+					</div>
+				</div>
 			</div>
 		</div>
 		<?php
