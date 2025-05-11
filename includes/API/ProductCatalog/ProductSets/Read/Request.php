0168dc6874ca763248e615fbe03cd14e4b37fdf2
Marian Shymon <marian.shymon@gmail.com>
2025-05-12 00:37:29 +0100

    Fix vendor/bin/phpcs errors.

--- includes/API/ProductCatalog/ProductSets/Read/Request.php
+++ includes/API/ProductCatalog/ProductSets/Read/Request.php
@@ -1,5 +1,6 @@
 <?php
-declare( strict_types=1 );
+
+declare(strict_types=1);
 
 namespace WooCommerce\Facebook\API\ProductCatalog\ProductSets\Read;
 
@@ -16,12 +17,12 @@ class Request extends ApiRequest {
 
 	/**
 	 * @param string $product_catalog_id Facebook Product Catalog ID.
-	 * @param array  $data Facebook Product Set Data.
+	 * @param string $retailer_id Facebook Product Set Retailer ID.
 	 */
 	public function __construct( string $product_catalog_id, string $retailer_id ) {
 		parent::__construct( "/{$product_catalog_id}/product_sets", 'GET' );
-        parent::set_params( array(
-            'retailer_id'     => $retailer_id,
-        ) );
+		parent::set_params(
+			array( 'retailer_id' => $retailer_id )
+		);
 	}
 }
