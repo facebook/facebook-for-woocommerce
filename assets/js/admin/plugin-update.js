cf8c1569ca126b55cd19dd02cee2ed1d3307efa6
Sayan Pandey <sayanpandey@meta.com>
2025-05-20 19:02:27 +0100

    Finalizing the opt out experience except the links and proper version

--- assets/js/admin/plugin-update.js
+++ assets/js/admin/plugin-update.js
@@ -9,7 +9,6 @@
 
 
 jQuery( document ).ready( function( $ ) {
-    
     // Opt out sync controls
      $('#opt_out_of_sync_button').on('click', function(event) {
         event.preventDefault();
@@ -20,23 +19,7 @@ jQuery( document ).ready( function( $ ) {
             data = typeof response === "string" ? JSON.parse(response) : response;
             if(data.success){
                 $('#opt_out_banner').hide();
-            }
-        }).fail(function() {
-            console.error("Error Code:", xhr.status);
-            console.error("Error Message:", xhr.responseText);
-        });
-    })
-
-    // Opt out sync controls
-    $('#opt_in_to_automatic_sync_button').on('click', function(event) {
-        event.preventDefault();
-        $.post( facebook_for_woocommerce_plugin_update.ajax_url, {
-            action: 'wc_facebook_opt_back_in_for_sync',
-            nonce:  facebook_for_woocommerce_plugin_update.opt_out_of_sync,
-        }, function (response){
-            data = typeof response === "string" ? JSON.parse(response) : response;
-            if(data.success){
-                $('#opt_in_banner').hide();
+                $('#opt_in_banner').show();
             }
         }).fail(function() {
             console.error("Error Code:", xhr.status);
