ead1c5b3b7b5627cfd50412e29d9a09cba3ae9b7
Sayan Pandey <sayanpandey@meta.com>
2025-05-23 15:40:22 +0100

    Removing plugin upgrade code

--- includes/Handlers/PluginUpdate.php
+++ includes/Handlers/PluginUpdate.php
@@ -32,9 +32,6 @@ class PluginUpdate {
 	const ACTION_OPT_OUT_OF_SYNC = 'wc_facebook_opt_out_of_sync';
 
 	/** @var string opt out sync action */
-	const ACTION_UPGRADE_PLUGIN = 'wc_facebook_upgrade_plugin';
-
-	/** @var string opt out sync action */
 	const ACTION_SYNC_BACK_IN = 'wc_facebook_sync_back_in';
 
 	/** @var string master sync option */
@@ -67,7 +64,6 @@ class PluginUpdate {
 				'ajax_url'                        => admin_url( 'admin-ajax.php' ),
 				'set_excluded_terms_prompt_nonce' => wp_create_nonce( 'set-excluded-terms-prompt' ),
 				'opt_out_of_sync'                 => wp_create_nonce( self::ACTION_OPT_OUT_OF_SYNC ),
-				'upgrade_plugin'                  => wp_create_nonce( self::ACTION_UPGRADE_PLUGIN ),
 				'sync_back_in'                    => wp_create_nonce( self::ACTION_SYNC_BACK_IN ),
 				'sync_in_progress'                => Sync::is_sync_in_progress(),
 				'opt_out_confirmation_message'    => self::get_opt_out_modal_message(),
@@ -80,8 +76,6 @@ class PluginUpdate {
 		add_action( 'admin_enqueue_scripts', [ __CLASS__,  'enqueue_assets' ] );
 		add_action( 'wp_ajax_wc_facebook_opt_out_of_sync', [ __CLASS__,  'opt_out_of_sync_clicked' ] );
 		add_action( 'wp_ajax_nopriv_wc_facebook_opt_out_of_sync', [ __CLASS__,'opt_out_of_sync_clicked' ] );
-		add_action( 'wp_ajax_wc_facebook_upgrade_plugin', [ __CLASS__,  'upgrade_facebook_woocommerce_plugin' ] );
-		add_action( 'wp_ajax_nopriv_wc_facebook_upgrade_plugin', [ __CLASS__,'upgrade_facebook_woocommerce_plugin' ] );
 		add_action( 'wp_ajax_wc_facebook_sync_all_products', [ __CLASS__,  'sync_all_clicked' ] );
 		add_action( 'wp_ajax_nopriv_wc_facebook_sync_all_products', [ __CLASS__,'sync_all_clicked' ] );
 	}
@@ -125,15 +119,6 @@ class PluginUpdate {
 		 */
 		if ( self::compare_versions( $latest_version, $current_version ) >= 0 && self::compare_versions( $latest_version, self::ALL_PRODUCTS_PLUGIN_VERSION ) < 0 ) {
 			add_action( 'admin_notices', [ __CLASS__, 'upcoming_version_change_banner' ], 0, 1 );
-		} elseif ( self::compare_versions( $latest_version, self::ALL_PRODUCTS_PLUGIN_VERSION ) >= 0 && self::compare_versions( $latest_version, $current_version ) > 0 ) {
-			/**
-			 * If latest version is above All products version show the update banner accordingly
-			 * also show for update banner in case latest version is above current version
-			 */
-			add_action( 'admin_notices', [ __CLASS__, 'plugin_update_available_banner' ], 0, 1 );
-		} elseif ( get_transient( 'show_plugin_updated_notice' ) ) {
-			add_action( 'admin_notices', [ __CLASS__, 'plugin_updated_banner' ] );
-			delete_transient( 'show_plugin_updated_notice' );
 		}
 	}
 
@@ -160,74 +145,6 @@ class PluginUpdate {
 		}
 	}
 
-	public function plugin_update_available_banner() {
-		$screen = get_current_screen();
-
-		if ( isset( $screen->id ) && 'marketing_page_wc-facebook' === $screen->id ) {
-			echo '<div id="opt_out_banner_update_available" class="' . esc_html( self::get_opt_out_banner_class() ) . '" style="padding: 15px">
-            <h2>Version ' . esc_html( self::get_latest_plugin_version() ) . ' is available: This release includes automatic product syncing</h2>
-            A new version of our plugin is now available. When you update, all your products will now be automatically synced to Meta for seamless advertising. Opt out before you update if want to sync your products later.
-                <p>
-                   <a href="javascript:void(0);" class="button wc-forward upgrade_plugin_button">
-                        Update now
-                    </a>
-                    <a href="javascript:void(0);" style="text-decoration: underline; cursor: pointer; margin-left: 10px" class="opt_out_of_sync_button"> Opt out of automatic sync</a>
-                </p>
-            </div>';
-
-			echo '<div id="opt_in_banner_update_available" class="' . esc_html( self::get_opt_in_banner_class() ) . '" style="padding: 15px">
-            <h2>Update to the latest plugin version </h2>
-                <p>
-                    A new version of our plugin is now available, featuring improved performance and simplified features. Since you’ve opted out of the automatic product sync, it will not be part of this update. Update now to get the best experience possible.
-                </p>
-                <p>
-                    <a href="javascript:void(0);" class="button wc-forward upgrade_plugin_button">
-                        Update now
-                    </a>
-                </p>
-            </div>';
-		}
-	}
-
-	public function plugin_updated_banner() {
-		$screen = get_current_screen();
-
-		if ( isset( $screen->id ) && 'marketing_page_wc-facebook' === $screen->id ) {
-
-			if ( self::is_master_sync_on() ) {
-				echo '<div class="notice notice-success is-dismissible" style="padding: 15px">
-                <h2>You’ve updated to the latest plugin version</h2>
-                    <p>
-                        As part of this update, all your products automatically sync to Meta. It may take some time before all your products are synced. If you change your mind, go to WooCommerce > Products and select which products to un-sync. <a href="https://www.facebook.com/business/help/4049935305295468"> About syncing products to Meta </a>
-                    </p>
-                </div>';
-			} else {
-				$hidden                 = self::is_master_sync_on();
-				$opted_out_banner_class = $hidden ? 'hidden' : '';
-				$opted_in_banner_class  = ! $hidden ? 'hidden' : '';
-
-				echo '<div id="opted_out_banner_updated_plugin" class="notice notice-success is-dismissible ' . esc_html( $opted_out_banner_class ) . '"" style="padding: 15px">
-                    <h2>You’ve updated to the latest plugin version</h2>   
-                        <p>
-                            To see all the changes, view the changelog. Since you’ve opted out of automatically syncing all your products, some of your products are not yet on Meta. We recommend turning on auto syncing to help drive your sales and improve ad performance. About syncing products to Meta
-                        </p>
-                        <p>
-                            <a href="javascript:void(0);" class="button wc-forward" id="sync_all_products">
-                                Sync all products
-                            </a>
-                        </p>
-                    </div>';
-
-				echo '<div id="opted_in_banner_updated_plugin" class="notice notice-success is-dismissible ' . esc_html( $opted_in_banner_class ) . '"" style="padding: 15px">
-                    <h2>Your products will be synced automatically</h2>   
-                        <p>
-                            It may take some time before all your products are synced. If you change your mind, go to WooCommerce > Products and select which products to un-sync.<a href="https://www.facebook.com/business/help/4049935305295468"> About syncing products to Meta</a>
-                        </p>
-                    </div>';
-			}
-		}
-	}
-
 	public function opt_out_of_sync_clicked() {
 		try {
 			$latest_date = gmdate( 'Y-m-d H:i:s' );
@@ -248,47 +165,6 @@ class PluginUpdate {
 			wp_send_json_error( 'Failed to opt in' );
 		}
 	}
-
-	public function upgrade_facebook_woocommerce_plugin() {
-		$plugin_slug = 'facebook-for-woocommerce';
-		$plugin_file = "$plugin_slug/$plugin_slug.php";
-
-		if ( ! function_exists( 'get_plugin_data' ) ) {
-			require_once ABSPATH . 'wp-admin/includes/plugin.php';
-		}
-
-		$plugin_data       = get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin_file );
-		$installed_version = $plugin_data['Version'];
-
-		$response = wp_remote_get( "https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&slug={$plugin_slug}" );
-
-		if ( is_wp_error( $response ) ) {
-			error_log( 'Error while upgrading plugin' );
-			wp_send_json_error( 'Failed to upgrade plugin' );
-		}
-
-		$plugin_info = json_decode( wp_remote_retrieve_body( $response ) );
-
-		if ( ! isset( $plugin_info->version ) ) {
-			error_log( 'Failed to fetch plugin version' );
-			wp_send_json_error( 'Failed to fetch plugin version' );
-		}
-
-		$latest_version = $plugin_info->version;
-
-		if ( version_compare( $installed_version, $latest_version, '<' ) ) {
-			include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-
-			$upgrader = new \Plugin_Upgrader( new \Automatic_Upgrader_Skin() );
-			$result   = $upgrader->upgrade( $plugin_file );
-			activate_plugin( $plugin_file );
-			return $result ? wp_send_json_success( 'Upgraded to lates version :' . $latest_version ) : wp_send_json_error( 'Upgrade failed failed!' );
-		}
-
-		set_transient( 'show_plugin_updated_notice', true, 1000 );
-		return wp_send_json_success( 'Plugin up to date' );
-	}
-
 
 	/**
 	 * Utils for this class
