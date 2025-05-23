9639f76ebb4b450ca677ec792ed1723ae981d7b0
David Evbodaghe <davidevbodaghe@meta.com>
2025-05-23 15:10:00 +0100

    second clean up

--- includes/Admin.php
+++ includes/Admin.php
@@ -1697,8 +1697,6 @@ class Admin {
 		?>
 		<script type="text/javascript">
 			jQuery(document).ready(function($) {
-				// Add CSS for synced fields
-				// CSS has been moved to facebook-for-woocommerce-products-admin.css
 				
 				// State object to track badge display status
 				var syncedBadgeState = {
