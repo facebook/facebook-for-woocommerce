08292e93053e27526acfdfc56079143961d01327
Sayan Pandey <sayanpandey@meta.com>
2025-05-23 15:40:22 +0100

    Removing plugin upgrade code

--- assets/js/admin/plugin-update.js
+++ assets/js/admin/plugin-update.js
@@ -47,53 +47,5 @@ jQuery( document ).ready( function( $ ) {
             }
         });
     })
-
-    $('.upgrade_plugin_button').on('click',function(event) {
-        event.preventDefault();
-        let context = $(this);
-        $.post( facebook_for_woocommerce_plugin_update.ajax_url, {
-            action: 'wc_facebook_upgrade_plugin',   
-            nonce:  facebook_for_woocommerce_plugin_update.upgrade_plugin,
-        } ,function (response){
-            data = typeof response === "string" ? JSON.parse(response) : response;
-            if( data.success ) {
-              location.reload();
-            }
-            else{
-                context.text('Failed to update plugin !')
-                context.css("color", "red");
-                context.css('border', '2px solid red');
-                context.prop('disabled', true);
-            }
-        }).fail(function(xhr) {
-            console.error("Error Code:", xhr.status);
-            console.error("Error Message:", xhr.responseText);
-        });
-    });
-
-    $('#sync_all_products').on('click',function(event) {
-        event.preventDefault();
-        let context = $(this);
-        $.post( facebook_for_woocommerce_plugin_update.ajax_url, {
-            action: 'wc_facebook_sync_all_products',   
-            nonce:  facebook_for_woocommerce_plugin_update.sync_back_in,
-        } ,function (response){
-            data = typeof response === "string" ? JSON.parse(response) : response;
-            if( data.success ) {
-                $('#opted_out_banner_updated_plugin').hide();
-                $('#opted_in_banner_updated_plugin').show();
-            }
-            else{
-                context.text('Failed to enable sync !')
-                context.css("color", "red");
-                context.css('border', '2px solid red');
-                context.prop('disabled', true);
-            }
-        }).fail(function(xhr) {
-            console.error("Error Code:", xhr.status);
-            console.error("Error Message:", xhr.responseText);
-        });
-    });
-
 });
 
