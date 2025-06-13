33df73242a988e1d6e963e0a89caef3298243642
Marko Ranđelović <marko@stuntcoders.com>
2025-06-13 12:46:16 +0200

    Skip sanitization checks for plugin cookies

--- includes/Events/Event.php
+++ includes/Events/Event.php
@@ -270,7 +270,7 @@ class Event {
 		} elseif ( ! empty( $_COOKIE['_fbc'] ) ) {
 			$fbc = wc_clean( wp_unslash( $_COOKIE['_fbc'] ) );
 		} elseif ( isset( $_SESSION['_fbc'] ) ) {
-			$fbc = $_SESSION['_fbc'];
+			$fbc = $_SESSION['_fbc']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 		}
 		if ( $fbc ) {
 			$_SESSION['_fbc'] = $fbc;
@@ -291,7 +291,7 @@ class Event {
 		if ( $fbp ) {
 			$_SESSION['_fbp'] = $fbp;
 		} elseif ( isset( $_SESSION['_fbp'] ) ) {
-			$fbp = $_SESSION['_fbp'];
+			$fbp = $_SESSION['_fbp']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 		}
 		return $fbp;
 	}
