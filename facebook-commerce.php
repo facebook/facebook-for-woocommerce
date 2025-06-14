5038d4c414dd446c69fd1416662395af6e64cd57
Sayan Pandey <sayanpandey@meta.com>
2025-06-14 18:25:56 +0100

    Making test fixes

--- facebook-commerce.php
+++ facebook-commerce.php
@@ -1195,7 +1195,6 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 		if ( ! $this->product_should_be_synced( $woo_product->woo_product ) ) {
 			return;
 		}
-
 		return $this->create_product_simple( $woo_product );  // new product
 	}
 
@@ -1276,6 +1275,10 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 	/**
 	 * Update existing product group (variant data only)
 	 *
+	 * @deprecated as we are no longer calling an update
+	 * We create everytime and it will be handled in the backend
+	 * TO_BE_DELETED
+	 * 
 	 * @param WC_Facebook_Product $woo_product
 	 **/
 	public function update_product_group( WC_Facebook_Product $woo_product ) {
@@ -2710,7 +2713,7 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 		}
 
 		$should_set_visible = self::FB_SHOP_PRODUCT_VISIBLE === $visibility;
-		if ( $product->is_type( 'variation' ) ) {
+		if ( $product->is_type( 'variation' ) || $product->is_type( 'simple' )) {
 			Products::set_product_visibility( $product, $should_set_visible );
 			$this->facebook_for_woocommerce->get_products_sync_handler()->create_or_update_products( [ $product->get_id() ] );
 		} elseif ( $product->is_type( 'variable' ) ) {
@@ -2731,24 +2734,6 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 			}
 			// sync product with all variations
 			$this->facebook_for_woocommerce->get_products_sync_handler()->create_or_update_products( $product_ids );
-		} else {
-			try {
-				$set_visibility = $this->facebook_for_woocommerce->get_api()->update_product_item( $fb_product_item_id, [ 'visibility' => $visibility ] );
-				if ( $set_visibility->success ) {
-					Products::set_product_visibility( $product, $should_set_visible );
-				}
-			} catch ( ApiException $e ) {
-				$message = sprintf( 'There was an error trying to update product item: %s', $e->getMessage() );
-				Logger::log(
-					$message,
-					[],
-					array(
-						'should_send_log_to_meta'        => false,
-						'should_save_log_in_woocommerce' => true,
-						'woocommerce_log_level'          => \WC_Log_Levels::ERROR,
-					)
-				);
-			}
 		}
 	}
 
