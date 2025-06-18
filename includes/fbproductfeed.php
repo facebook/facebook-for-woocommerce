3c0d34872650df5c8de22dfec982cee9cc545105
Sayan Pandey <sayanpandey@meta.com>
2025-06-18 12:47:58 +0100

    Adding warning fix

--- includes/fbproductfeed.php
+++ includes/fbproductfeed.php
@@ -543,7 +543,7 @@ class WC_Facebook_Product_Feed {
 		}
 
 		// Setting up Woo All Products sync flag
-		$is_woo_all_products_sync = $product_data['is_woo_all_products_sync'] || 0;
+		$is_woo_all_products_sync = isset($product_data['is_woo_all_products_sync']) ? $product_data['is_woo_all_products_sync'] : 0;
 
 		return $product_data['retailer_id'] . ',' .
 		static::format_string_for_feed( static::get_value_from_product_data( $product_data, 'name' ) ) . ',' .
