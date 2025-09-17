334446cf6433c3aa2b3b0c37b0a32d21c277e359
Cheng Hao Chen <chenghaochen@meta.com>
2025-09-17 11:01:58 -0400

    param builder set cookie order fix

--- facebook-commerce-events-tracker.php
+++ facebook-commerce-events-tracker.php
@@ -108,7 +108,7 @@ if ( ! class_exists( 'WC_Facebookcommerce_EventsTracker' ) ) :
 		 * @since 2.2.0
 		 */
 		private function add_hooks() {
-			add_action( 'init', array( $this, 'param_builder_set_cookies' ), 15 );
+			add_action( 'init', array( $this, 'param_builder_set_cookies' ) );
 
 			// inject Pixel
 			add_action( 'wp_head', array( $this, 'inject_base_pixel' ) );
