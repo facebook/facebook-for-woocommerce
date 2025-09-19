ac2bcfbaae9ce37cb44a0d9d1ba6ab83b29b79ad
Cheng Hao Chen <chenghaochen@meta.com>
2025-09-19 07:47:24 -0400

    restore get_integration order

--- class-wc-facebookcommerce.php
+++ class-wc-facebookcommerce.php
@@ -191,7 +191,7 @@ class WC_Facebookcommerce extends WooCommerce\Facebook\Framework\Plugin {
 	 */
 	public function init() {
 		add_action( 'init', array( $this, 'init_param_builder' ), 1 );
-		add_action( 'init', array( $this, 'get_integration' ), 16 );
+		add_action( 'init', array( $this, 'get_integration' ) );
 
 		add_action( 'woocommerce_init', array( $this, 'add_whatsapp_consent_block_checkout_fields' ) );
 		add_filter( 'woocommerce_checkout_fields', array( $this, 'add_whatsapp_consent_classic_checkout_fields' ) );
