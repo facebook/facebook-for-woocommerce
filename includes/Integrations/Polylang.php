ca448e58e02ccc47140a67f84104bb4e7c5d8bbc
Rafael Louis Curran <rcurran@meta.com>
2025-11-18 11:18:54 -0500

    type checking error

--- includes/Integrations/Polylang.php
+++ includes/Integrations/Polylang.php
@@ -146,7 +146,8 @@ class Polylang extends Abstract_Localization_Integration {
 		}
 
 		// Use Polylang function to get product language slug
-		return pll_get_post_language( $product_id );
+		$language = pll_get_post_language( $product_id );
+		return $language !== false ? $language : null;
 	}
 
 	/**
