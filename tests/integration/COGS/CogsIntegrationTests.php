1645e97cc9574f19cc1cb3a09f9b3adc7ff8e235
Vahid Kaykhaei <vahidkay@meta.com>
2025-12-04 10:24:59 +0100

    Updated Integration tests

--- tests/integration/COGS/CogsIntegrationTests.php
+++ tests/integration/COGS/CogsIntegrationTests.php
@@ -46,8 +46,8 @@ class CogsIntegrationTests extends IntegrationTestCase
 		$this->assertTrue(WooCCogsProvider::is_available());
 
 		$product = $this->create_simple_product();
-		$product->update_post_meta('_cost_of_goods', floatval(100.0));
-		$product->update_post_meta('cogs_value', floatval(200.0));
+		$product->update_meta_data('_cost_of_goods', floatval(100.0));
+		$product->update_meta_data('cogs_value', floatval(200.0));
 		$product->save();
 
 		$this->assertTrue($product->get_cogs_value() == null, 'cogs is not enabled at a product level');
