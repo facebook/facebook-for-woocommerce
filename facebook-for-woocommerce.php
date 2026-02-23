cc61006c5f4f6f0171f76086e453300c3ba60579
Rafael Louis Curran <rcurran@meta.com>
2026-02-23 12:09:45 -0800

    Release/3.5.17 (#3867)
    Summary:
    * Fix - Fix - Removed the prefix from retailer id. by vahidkay-meta in https://github.com/facebook/facebook-for-woocommerce/issues/3858
    * Add - [WooCommernce] [Rich Order] Add rich-order payload gating and simplify per-item amount for events. by ashutoshbondre in https://github.com/facebook/facebook-for-woocommerce/issues/3848
    * Add - Fix pixel event tracking by isolating JS execution context by cshing-meta in https://github.com/facebook/facebook-for-woocommerce/issues/3835
    * Dev - Tweak - Broke down integration tests to run with PHP7.4 & PHP8.4 by vahidkay-meta in https://github.com/facebook/facebook-for-woocommerce/issues/3855
    * Fix - Tweak - Added logging to detect if we can reliably use fbcollection by vahidkay-meta in https://github.com/facebook/facebook-for-woocommerce/issues/3854
    * Fix - Fix duplicate CAPI Purchase events during checkout process by cshing-meta in https://github.com/facebook/facebook-for-woocommerce/issues/3850
    * Tweak - Optimize Release process, automate marketplace artifact verification by vahidkay-meta in https://github.com/facebook/facebook-for-woocommerce/issues/3846
    * Fix - Fix performance issue: Cache background sync job queries and skip on … by devbodaghe in https://github.com/facebook/facebook-for-woocommerce/issues/3823
    * Fix - Fix test with accurate retailer ID by jarretth in https://github.com/facebook/facebook-for-woocommerce/issues/3852
    * Fix - Pin Polylang version for PHP 7.4 in CI by jarretth in https://github.com/facebook/facebook-for-woocommerce/issues/3851
    * Tweak - Update parambuilder server version to 1.2.1 by jarretth in https://github.com/facebook/facebook-for-woocommerce/issues/3844
    
    Pull Request resolved: https://github.com/facebook/facebook-for-woocommerce/pull/3867
    
    Reviewed By: vahidkay-meta
    
    Differential Revision: D93779268
    
    Pulled By: rafael-curran
    
    fbshipit-source-id: c60bb5c76fa5d96dc6aaaa68574caf8852833f72

--- facebook-for-woocommerce.php
+++ facebook-for-woocommerce.php
@@ -10,7 +10,7 @@
  * Description: Grow your business on Meta platforms! Use this official plugin to help sell more of your products using Facebook and Instagram. After completing the setup, you'll be ready to create ads that promote your products and you can also create a shop section on your Page where customers can browse your products.
  * Author: Meta
  * Author URI: https://www.meta.com/
- * Version: 3.5.16
+ * Version: 3.5.17
  * Requires at least: 5.6
  * Requires PHP: 7.4
  * Text Domain: facebook-for-woocommerce
@@ -62,7 +62,7 @@ class WC_Facebook_Loader {
 	/**
 	 * @var string the plugin version. This must be in the main plugin file to be automatically bumped by Woorelease.
 	 */
-	const PLUGIN_VERSION = '3.5.16'; // WRCS: DEFINED_VERSION.
+	const PLUGIN_VERSION = '3.5.17'; // WRCS: DEFINED_VERSION.
 
 	// Minimum PHP version required by this plugin.
 	const MINIMUM_PHP_VERSION = '7.4.0';
